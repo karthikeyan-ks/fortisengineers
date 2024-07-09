6d402284fe4dc7f1d4d2c2a0032b412232950798
karthikeyan-ks <karthikeyan3673@gmail.com>
2024-07-09 10:57:01 +0530

    change in project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { AppBar } from './component/Appbar'
+
+function App() {
+  return (  
+    <>
+      <AppBar/>
+      <div>
+          <section>
+
+          </section>
+          <section>
+            
+          </section>
+          <section>
+            
+          </section>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Appbar.tsx
@@ -0,0 +1,11 @@
+import './Appbar.css'
+export const AppBar = () =>{
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}                   
